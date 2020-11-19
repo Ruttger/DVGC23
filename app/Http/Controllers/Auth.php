892d60f4821f693f03722761e80f7d389b4c41d4
rydhzan <rydh.anton0@gmail.com>
2020-11-19 18:21:13 +0100

    Login view, with controll

---
+++ app/Http/Controllers/Auth.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Auth extends Controller
+{
+    //
+    function Login(Request $req){
+    	$username = $req->input('username');
+    	$password = $req->input('password');
+
+		$query = DB::table('users')->select('id')->where('username', $username)->get();
+    	// $id = DB::select('id')->where('username', $username)->first();
+    	if(!$query->isEmpty()){
+	    	return "welcome"; 
+    	}
+    	else{
+    		return redirect()->back()->with('error', 'Fel lösenord och <br>eller användarnamn!');
+    	}
+    	// print_r($query[0]->id);
+ 	  	// return $req->input();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/laravel', function () {
     return view('welcome');
 });
+
+Route::post("user", [Auth::class, 'Login']);
